0897f5a082a0eec305b95ef8fbf22697f824f8ad
ArhumRashid786 <arhumr93@gmail.com>
2024-05-20 14:18:24 -0700

    updated 20/5/2024

--- first.js
+++ first.js
@@ -10,15 +10,44 @@ student["age"] = 77;
 console.log(student);
 
 
-const student = {
+const teacher = {
     name : "quaid",
     age: 44,
     marks: 55,
     isFollow: true,
 };
 
-student["age"] = 77;
-console.log(student.age);
+teacher["age"] = 77;
+console.log(teacher.age);
+
+const product = {
+    name: "pen",
+    rating: 5,
+    offer: "5%",
+    price: 800,
+}
+
+product["name"] = "shado",
+product["price"] = 400,
+
+console.log(product);
+
+let a = 12;
+let b = 19;
+
+results = (a+b);
+
+console.log(results);  
+
+let c = 70;
+let f = 30;
+let g = c+f;
+
+console.log("yello + b = ", g);
+console.log ("a % b = ", c % f );
+
+c++;
+console.log ("the value of ++C is = ", c)
 
 
 
