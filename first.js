1648957197e0778f2b1826803965d74c4c920c43
ArhumRashid786 <arhumr93@gmail.com>
2024-05-20 12:16:45 -0700

    20/5/2024

---
+++ first.js
@@ -0,0 +1,26 @@
+const student = {
+    name: "jafar",
+    age: 55,
+    cgpa: 8.9,
+}
+
+student["name"] = "ullu";
+student["age"] = 77;
+
+console.log(student);
+
+
+const student = {
+    name : "quaid",
+    age: 44,
+    marks: 55,
+    isFollow: true,
+};
+
+student["age"] = 77;
+console.log(student.age);
+
+
+
+
+
