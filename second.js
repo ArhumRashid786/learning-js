b5df082cda5ca9abe302de81c9ee98cc6aba1514
ArhumRashid786 <arhumr93@gmail.com>
2024-05-21 15:02:58 -0700

    21/5/2024

---
+++ second.js
@@ -0,0 +1,69 @@
+// comparision operators
+
+// let a = 5;
+// let b = 2;
+// let c = 5;
+
+// console.log("a equal b", a==b);
+// console.log("a not equal b", a!=b);
+
+// console.log("a not equal c", a!=c);
+// console.log("a eqaul c", a==c);
+
+// console.log("a <= b", a <= b);
+
+// let h = "3";
+// let g = 3;
+
+// console.log ("h = g", h!==g);
+
+// console.log ("h > g", h >= g);
+
+
+//Logical Operators 
+
+// let a  = 3;
+// let b = 9;
+
+// console.log ("logical operator a < b && b === 9", a > b && b === 9, a > b || b === 9,
+// !(a < b)
+// );
+
+
+// conditional statement 
+
+let age = 15;
+
+if (age < 19){
+    console.log ("you can vote");
+}else{
+    console.log("not vote");
+}
+
+// if (age < 19) {
+//     console.log ("you can not vote");
+// }
+
+let mode = "black";
+let color;
+
+if (mode === "dark") {
+    color = "black";
+}else{
+    color = "pink";
+}
+
+// if (mode !== "dark") {
+//     color = "white";
+// }
+
+console.log (color);
+
+
+let num = 7;
+
+if(num%2 === 0){
+    console.log(num, "is even");
+}else{
+    console.log(num, "is odd");
+}
